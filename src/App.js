b5624987de037fbcb1eb2e9063b5dd76c432c6af
Abraham Dulla <abrish.d143t@gmail.com>
2022-06-24 15:04:29 +0300

    netlify test

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a>
+        <a href=''>
           Monster Rolodex
         </a>
       </header>
